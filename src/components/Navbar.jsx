8bec96efd22699b8db579d379fc391818675fa14
ImAmlan03 <imamlan03@gmail.com>
2023-10-02 12:20:44 +0530

    initialized tailwind, finalized color schemes

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div className="flex w-screen space justify-between p-10 bg-background font-bigjohn">
+            <div className='text-text text-2xl'>Amlan Saikia</div>
+            <div className='text-text'>Right</div>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        text: 'var(--text)',
+        background: 'var(--background)',
+        primary: 'var(--primary)',
+        secondary: 'var(--secondary)',
+        accent: 'var(--accent)',
+      },
+      fontFamily: {
+        bigjohn: ["Big John"],
+        slimjoe: ["Slim Joe"],
+      },
+    },
+  },
+  darkMode: 'class', 
+  plugins: [],
+}
+
